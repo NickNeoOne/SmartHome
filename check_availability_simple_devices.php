0a55dfa1f1e23c76963e205ee2db150e2b6483d2
NickNeo <33730384+NickNeoOne@users.noreply.github.com>
2022-11-04 02:29:49 +0400

    Update check_availability_simple_devices.php

--- check_availability_simple_devices.php
+++ check_availability_simple_devices.php
@@ -77,7 +77,7 @@ foreach($objRooms as $objr) {
        echo ' </tr>'; echo "\n";
       }  
     // Получаем время последнего обновления 
-    $updt=date("d.m.Y  H:i", $objs->getProperty('updated'));
+    $updt=date("d.m.Y  H:i", ((int)$objs->getProperty('updated')));
       // Разный цвет текста
        if ($objs->getProperty('alive')==1) { $cn='<font title="Устройство доступно" class="green">'; $ce='</font>'; $dev_alive='<span>✓</span>';}
        elseif ($objs->getProperty('alive')==NULL ) { 
